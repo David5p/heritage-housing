015b3f574c322fbe3f0441715085de5a9c30dcde
David5p <k9glpuwd@students.codeinstitute.net>
2026-04-18 15:33:19 +0200

    Create basic structure for app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
-import pandas as pd
-import joblib
+from app_pages.multipage import MultiPage
 
-# Load model
-model = joblib.load(
-    "outputs/ml_pipeline/predict_saleprice/v1_gb_final/model_pipeline.pkl")
+from app_pages.page_summary import page_summary_body
+from app_pages.page_eda import page_eda_body
+from app_pages.page_predict import page_predict_body
+
+app = MultiPage(app_name="House Price Predictor")
+
+app.add_page("Project Summary", page_summary_body)
+app.add_page("Data Exploration", page_eda_body)
+app.add_page("Predict House Price", page_predict_body)
+
+app.run()
