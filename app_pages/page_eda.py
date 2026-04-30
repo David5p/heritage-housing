aeb2a81e7220a957ff1f6724ce47dd2473cb08c9
David5p <k9glpuwd@students.codeinstitute.net>
2026-04-30 09:36:27 +0200

    Add plots to eda dashboard page

--- app_pages/page_eda.py
+++ app_pages/page_eda.py
@@ -1,3 +1,5 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
 
@@ -9,10 +11,44 @@ def page_eda_body():
 
     df = pd.read_csv("outputs/datasets/cleaned/CleanedData.csv")
 
-    st.write("Dataset preview:")
+    st.write("Dataset preview")
     st.dataframe(df.head())
 
-    st.write("Shape:", df.shape)
+    st.write("Shape of dataset")
+    st.write(df.shape)
 
-    st.write("Summary statistics:")
+    st.write("Summary statistics")
     st.dataframe(df.describe())
+
+    st.write("Distribution of SalePrice")
+    plt.figure(figsize=(8, 5))
+    sns.histplot(df["SalePrice"], kde=True)
+    plt.title("SalePrice Distribution")
+    st.pyplot(plt)
+
+    st.write("""
+    **Insight:** SalePrice is right-skewed, meaning there are more lower-priced houses 
+    and fewer very expensive houses in the dataset.
+    """)
+
+    st.write("GrLivArea vs SalePrice")
+    plt.figure(figsize=(8, 5))
+    sns.scatterplot(data=df, x="GrLivArea", y="SalePrice")
+    plt.title("GrLivArea vs SalePrice")
+    st.pyplot(plt)
+
+    st.write("""
+    **Insight:** Larger above-ground living area generally leads to higher sale price, 
+    confirming that house size is a major price feature.
+    """)
+
+    st.write("YearBuilt vs SalePrice")
+    plt.figure(figsize=(8, 5))
+    sns.scatterplot(data=df, x="YearBuilt", y="SalePrice")
+    plt.title("YearBuilt vs SalePrice")
+    st.pyplot(plt)
+
+    st.write("""
+    **Insight:** Newer houses have a tendancy to sell for more, but the relationship is weaker 
+    than size-based features.
+    """)
