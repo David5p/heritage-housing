0ebfb88c4639570fc08542cc3179f9b63da9c473
David5p <k9glpuwd@students.codeinstitute.net>
2026-04-30 09:57:26 +0200

    Add median values to other features in predictor to improve reliability

--- app_pages/page_predict.py
+++ app_pages/page_predict.py
@@ -3,7 +3,13 @@ import pandas as pd
 import joblib
 
 
-# Cached model + features load
+# Cached training data model + features load
+
+@st.cache_data
+def load_training_data():
+    return pd.read_csv("outputs/ml_pipeline/predict_saleprice/v1_gb_final/X_train.csv")
+
+
 @st.cache_resource
 def load_model():
     return joblib.load(
@@ -23,9 +29,12 @@ def page_predict_body():
     Displays an interactive form to predict house prices using a trained ML model.
     """
 
-    # Load model and features
+    # Load model, features and training dataset
     model = load_model()
     feature_list = load_features()
+    df_train = load_training_data()
+
+    baseline_values = df_train.median(numeric_only=True)
 
     st.title("Predict House Price")
     st.write("Enter house details below:")
@@ -40,6 +49,11 @@ def page_predict_body():
     input_data = pd.DataFrame(columns=feature_list)
     input_data.loc[0] = 0
 
+    # Fill numeric columns with median baseline values
+    for col in baseline_values.index:
+        if col in input_data.columns:
+            input_data.at[0, col] = baseline_values[col]
+
     input_data.at[0, "GrLivArea"] = gr_liv_area
     input_data.at[0, "YearBuilt"] = year_built
     input_data.at[0, "GarageArea"] = garage_area
@@ -47,16 +61,16 @@ def page_predict_body():
 
     # Prediction button + validation
     if st.button("Predict Price"):
-
-        # validation
-        if gr_liv_area == 0 or garage_area == 0 or total_bsmt_sf == 0:
-            st.warning("Please enter realistic values before predicting.")
+        if gr_liv_area < 300:
+            st.warning(
+                "GrLivArea seems too low. Please enter a realistic value.")
         else:
             prediction = model.predict(input_data)
-
             st.success(f"Estimated Price: ${prediction[0]:,.0f}")
 
             st.caption(
-                "Note: This model is trained on U.S. housing data and provides estimates based on historical patterns for houses in Ames, Iowa. "
-                "It is not intended for real-world property valuation."
+                "Note: The prediction is based on the values you entered. "
+                "All other missing variables are automatically set to typical (median) values "
+                "from the training dataset. The model is trained on housing data from "
+                "Ames, Iowa and provides statistical estimates, not real-world valuations."
             )
